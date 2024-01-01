f2151906201cb7551540c97f72592ea141b9ca10
suryaofficial7 <suryaskofficial7@gmail.com>
2024-01-01 21:03:26 +0530

    many routes are setuped and hbs and static files setted express.statics and views , hbs

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-
-const path = require('path');
-const express = require('express');
-const hbs = require('handlebars');
+const path = require("path");
+const express = require("express");
+const hbs = require("handlebars");
 //? ===========================================================================================================
-// ! Initializations 
+// ! Initializations
 const app = express();
 const port = 8888;
 
 //? ===========================================================================================================
 
 // !Express INIT:)
-const staticPath = path.join(__dirname,"/public");
+const staticPath = path.join(__dirname, "/public");
 app.use(express.static(staticPath));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 //? ===========================================================================================================
-
 
 //! [ The root Page ]
 // ! [ I am Using Arrow Function ]
 
-app.get("/",(req,res)=>{
- res.send("index.html");
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+
+
+
 
 
 
 //? ===========================================================================================================
-// ! [ Lising to port ] 
-app.listen(port,()=>{
-    console.log("Running Now .....")
-})
+// ! [ LOGINS  ]
+app.get("/student/studentLogin", (req, res) => {
+  res.render("student/studentLogin");
+});
+
+app.get("/teacher/teacherLogin", (req, res) => {
+  res.render("teacher/teacherLogin");
+});
+
+app.get("admin/adminLogin", (req, res) => {
+  res.render("admin/adminLogin");
+});
+
+app.get("sudoUser/sudoUserLogin", (req, res) => {
+  res.render("sudoUser/sudoUserLogin");
+});
+
+
+
+
+
+
+//? ===========================================================================================================
+// ! [ Lising to port ]
+app.listen(port, () => {
+  console.log("Running Now .....");
+});
